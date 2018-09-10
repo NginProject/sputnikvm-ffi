5d5f070271d9545cd61543d90ebe021e8c73dede
NginProject <42475871+NginProject@users.noreply.github.com>
2018-09-10 08:01:40 -0500

    Update main.go

--- go/main.go
+++ go/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"math/big"
-	"github.com/ethereumproject/go-ethereum/common"
-	"github.com/ethereumproject/sputnikvm-ffi/go/sputnikvm"
+	"github.com/NginProject/ngind/common"
+	"github.com/NginProject/sputnikvm-ffi/go/sputnikvm"
 )
 
 func main() {
